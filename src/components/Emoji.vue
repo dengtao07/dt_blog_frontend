970b65645ef19b84a1696d91d11a5e0c27708aac
dengtao <dtqwe@qq.com>
2020-03-10 23:58:06 +0800

    将emoji替换为png插入，改善显示效果

--- src/components/Emoji.vue
+++ src/components/Emoji.vue
@@ -1,90 +1,90 @@
 <template>
-	<div>
-		<el-popover placement="bottom" width="320" trigger="click">
-			<div class="emoji-container">
-				<img
-					class="emoji"
-					v-for="(emoji, index) of emojiList"
-					:key="index"
-					:src="emojiUrlObj[emoji.filename]"
-					@click="exportEmojiUnicode(emoji.alt)"
-				/>
-			</div>
-			<img
-				ref="emojiBtn"
-				class="emoji-btn"
-				src="../assets/emoji/1f60a.png"
-				@mouseover="handleMouseover"
-				slot="reference"
-			/>
-		</el-popover>
-	</div>
+  <div>
+    <el-popover placement="bottom" width="320" trigger="click">
+      <div class="emoji-container">
+        <img
+          class="emoji"
+          v-for="(emoji, index) of emojiList"
+          :key="index"
+          :src="emojiUrlObj[emoji.filename]"
+          @click="exportEmoji(emoji)"
+        />
+      </div>
+      <img
+        ref="emojiBtn"
+        class="emoji-btn"
+        src="../assets/emoji/1f60a.png"
+        @mouseover="handleMouseover"
+        slot="reference"
+      />
+    </el-popover>
+  </div>
 </template>
 
 <script>
-import EmojiObj from '../utils/EmojiMapper.js';
+import EmojiObj from "../utils/EmojiMapper.js";
 export default {
-	name: 'Emoji',
-	data() {
-		return {
-			emojiList: EmojiObj.emojiList
-		};
-	},
-	computed: {
-		emojiUrlObj() {
-			let emojiUrlObj = {};
-			this.emojiList.forEach((item) => {
-				let filename = item.filename;
-				emojiUrlObj[filename] = require('../assets/emoji/' + filename + '.png');
-			});
-			return emojiUrlObj;
-		}
-	},
-	methods: {
-		exportEmojiUnicode(emoji) {
-			this.$emit('exportEmojiUnicode', emoji);
-		},
-		randomEmojiArr() {
-			let arr = [
-				require('../assets/emoji/1f600.png'),
-				require('../assets/emoji/1f601.png'),
-				require('../assets/emoji/1f609.png'),
-				require('../assets/emoji/1f61b.png'),
-				require('../assets/emoji/1f92a.png'),
-				require('../assets/emoji/1f973.png'),
-				require('../assets/emoji/1f638.png'),
-				require('../assets/emoji/1f920.png'),
-				require('../assets/emoji/1f917.png'),
-				require('../assets/emoji/1f923.png')
-			];
-			let randomIndex = Math.floor(Math.random() * 10);
-			this.$refs.emojiBtn.src = arr[randomIndex];
-		},
-		handleMouseover() {
-			this.randomEmojiArr();
-		}
-	}
+  name: "Emoji",
+  data() {
+    return {
+      emojiList: EmojiObj.emojiList
+    };
+  },
+  computed: {
+    emojiUrlObj() {
+      let emojiUrlObj = {};
+      this.emojiList.forEach(item => {
+        let filename = item.filename;
+        emojiUrlObj[filename] = require("../assets/emoji/" + filename + ".png");
+      });
+      return emojiUrlObj;
+    }
+  },
+  methods: {
+    exportEmoji(emoji) {
+      this.$emit("exportEmoji", emoji);
+    },
+    randomEmojiArr() {
+      let arr = [
+        require("../assets/emoji/1f600.png"),
+        require("../assets/emoji/1f601.png"),
+        require("../assets/emoji/1f609.png"),
+        require("../assets/emoji/1f61b.png"),
+        require("../assets/emoji/1f92a.png"),
+        require("../assets/emoji/1f973.png"),
+        require("../assets/emoji/1f638.png"),
+        require("../assets/emoji/1f920.png"),
+        require("../assets/emoji/1f917.png"),
+        require("../assets/emoji/1f923.png")
+      ];
+      let randomIndex = Math.floor(Math.random() * 10);
+      this.$refs.emojiBtn.src = arr[randomIndex];
+    },
+    handleMouseover() {
+      this.randomEmojiArr();
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
 .emoji-container {
-	height: 200px;
-	overflow: auto;
+  height: 200px;
+  overflow: auto;
 }
 
 .emoji {
-	width: 24px;
-	height: 24px;
-	margin: 6px;
-	&:hover {
-		cursor: pointer;
-	}
+  width: 24px;
+  height: 24px;
+  margin: 6px;
+  &:hover {
+    cursor: pointer;
+  }
 }
 
 .emoji-btn {
-	width: 24px;
-	height: 24px;
-	cursor: pointer;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 }
 </style>
--- src/components/EmojiInput.vue
+++ src/components/EmojiInput.vue
@@ -1,159 +1,177 @@
 <template>
-	<div>
-		<div class="edit-area">
-			<el-input
-				type="textarea"
-				:rows="3"
-				placeholder="畅所欲言吧"
-				v-model="textarea"
-				maxlength="200"
-				show-word-limit
-			></el-input>
-			<emoji class="emoji-picker" @exportEmojiUnicode="getEmojiCode"></emoji>
-		</div>
-		<div v-if="!username" class="info">
-			<el-form
-				class="info-form"
-				label-width="80px"
-				:model="userInfoForm"
-				ref="userInfoForm"
-			>
-				<el-form-item label="您的大名" prop="username">
-					<el-input
-						type="text"
-						v-model="userInfoForm.username"
-						maxlength="10"
-						autocomplete="off"
-						placeholder="必填"
-					></el-input>
-				</el-form-item>
-				<el-form-item label="联系方式" prop="contact">
-					<el-input
-						type="text"
-						v-model="userInfoForm.contact"
-						autocomplete="off"
-						placeholder="微信/QQ/邮箱，必填"
-					></el-input>
-				</el-form-item>
-			</el-form>
-		</div>
-		<div class="action">
-			<span v-if="!username">请先注册登录或留下您的基本信息</span>
-			<el-button
-				type="primary"
-				size="medium"
-				:disabled="!submitAble"
-				icon="el-icon-s-promotion"
-				:loading="submittingComment"
-				@click="submitComment"
-				>发送</el-button
-			>
-		</div>
-	</div>
+  <div>
+    <div class="edit-area">
+      <div
+        class="textarea"
+        ref="inputContent"
+        contenteditable="true"
+        autocomplete="off"
+        :placeholder="placeholder"
+        draggable="false"
+        spellcheck="false"
+      ></div>
+      <emoji class="emoji-picker" @exportEmoji="insertEmojiImg"></emoji>
+    </div>
+    <div v-if="!username" class="info">
+      <el-form class="info-form" label-width="80px" :model="userInfoForm" ref="userInfoForm">
+        <el-form-item label="您的大名" prop="username">
+          <el-input
+            type="text"
+            v-model="userInfoForm.username"
+            maxlength="10"
+            autocomplete="off"
+            placeholder="必填"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="联系方式" prop="contact">
+          <el-input
+            type="text"
+            v-model="userInfoForm.contact"
+            autocomplete="off"
+            placeholder="微信/QQ/邮箱，必填"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="action">
+      <span v-if="!username">请先注册登录或留下您的基本信息</span>
+      <el-button
+        type="primary"
+        size="medium"
+        :disabled="!submitAble"
+        icon="el-icon-s-promotion"
+        :loading="submittingComment"
+        @click="submitComment"
+      >发送</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-import Emoji from './Emoji';
-import { mapMutations } from 'vuex';
-import { showMessage, formatedDate } from '../utils/utils.js';
+import Emoji from "./Emoji";
+import { mapMutations } from "vuex";
+import { showMessage, formatedDate } from "../utils/utils.js";
 export default {
-	name: 'EmojiInput',
-	props: ['index'],
-	data() {
-		return {
-			textarea: '',
-			userInfoForm: {
-				username: '',
-				contact: ''
-			},
-			comIndex: this.index,
-			submittingComment: false
-		};
-	},
-	components: {
-		Emoji
-	},
-	methods: {
-		...mapMutations(['changeCommentSubmitState']),
-		getEmojiCode(emojiUnicode) {
-			this.textarea += emojiUnicode;
-		},
-		async submitComment() {
-			let commentInfo = {
-				author: this.username || this.userInfoForm.username,
-				content: this.textarea,
-				articleId: this.comIndex.articleId.split('/')[2],
-				commentId: this.comIndex.commentId,
-				replyTo: this.comIndex.replyTo
-			};
-			this.submittingComment = true;
-			let response = await this.$apis.saveComment(commentInfo);
-			if (response && response.data && response.data.affectedRows === 1) {
-				this.textarea = '';
-				!this.username && this.$refs['userInfoForm'].resetFields();
-				this.changeCommentSubmitState();
-				this.$emit('closeEmojiInput');
-				showMessage('提交成功', 'success');
-			} else {
-				showMessage('提交失败，请稍后再试', 'error');
-			}
-			this.submittingComment = false;
-		}
-	},
-	computed: {
-		username() {
-			return this.$store.state.username;
-		},
-		submitAble() {
-			let userInfor = this.userInfoForm;
-			let isUserInfoAble =
-				!!userInfor.username.trim() && !!userInfor.contact.trim();
-			let isVuexUsernameAble = !!this.username;
-			let isTextAreaAble = !!this.textarea.trim();
-			return (isVuexUsernameAble || isUserInfoAble) && isTextAreaAble;
-		}
-	}
+  name: "EmojiInput",
+  props: ["index"],
+  data() {
+    return {
+      textarea: "",
+      userInfoForm: {
+        username: "",
+        contact: ""
+      },
+      placeholder: "畅所欲言吧...",
+      comIndex: this.index,
+      submittingComment: false
+    };
+  },
+  components: {
+    Emoji
+  },
+  methods: {
+    ...mapMutations(["changeCommentSubmitState"]),
+    insertEmojiImg(emoji) {
+      let emojiImg = document.createElement("img");
+      emojiImg.src = emoji.src;
+      emojiImg.alt = emoji.alt;
+      emojiImg.setAttribute(
+        "style",
+        "height:20px; width:20px; draggable:false; margin:0px 1px"
+      );
+      this.$refs.inputContent.appendChild(emojiImg);
+    },
+    async submitComment() {
+      let commentInfo = {
+        author: this.username || this.userInfoForm.username,
+        content: this.$refs.inputContent.innerHTML,
+        articleId: this.comIndex.articleId.split("/")[2],
+        commentId: this.comIndex.commentId,
+        replyTo: this.comIndex.replyTo
+      };
+      this.submittingComment = true;
+      let response = await this.$apis.saveComment(commentInfo);
+      if (response && response.data && response.data.affectedRows === 1) {
+        this.$refs.inputContent.innerHTML = "";
+        !this.username && this.$refs["userInfoForm"].resetFields();
+        this.changeCommentSubmitState();
+        this.$emit("closeEmojiInput");
+        showMessage("提交成功", "success");
+      } else {
+        showMessage("提交失败，请稍后再试", "error");
+      }
+      this.submittingComment = false;
+    }
+  },
+  computed: {
+    username() {
+      return this.$store.state.username;
+    },
+    submitAble() {
+      let userInfor = this.userInfoForm;
+      let isUserInfoAble =
+        !!userInfor.username.trim() && !!userInfor.contact.trim();
+      let isVuexUsernameAble = !!this.username;
+      let isTextAreaAble =
+        this.$refs.inputContent && !!this.$refs.inputContent.innerHTML.trim();
+      return (isVuexUsernameAble || isUserInfoAble) && isTextAreaAble;
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
 .edit-area {
-	margin-bottom: 10px;
-	position: relative;
-	.emoji-picker {
-		position: absolute;
-		height: 24px;
-		width: 24px;
-		bottom: 5px;
-		left: 10px;
-	}
-	::v-deep {
-		.el-textarea__inner {
-			padding-bottom: 30px;
-		}
-	}
+  margin-bottom: 10px;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  position: relative;
+  .textarea {
+    border-radius: 4px;
+    padding: 8px 20px 35px 20px;
+    font-size: 14px;
+    &:empty:before {
+      content: attr(placeholder);
+      position: absolute;
+      opacity: 0.4;
+      pointer-events: none;
+      user-select: none;
+    }
+  }
+  .emoji-picker {
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    bottom: 5px;
+    left: 10px;
+  }
+  ::v-deep {
+    .el-textarea__inner {
+      padding-bottom: 30px;
+    }
+  }
 }
 
 .info {
-	.info-form {
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 10px;
-		.el-form-item {
-			flex-basis: 48%;
-			margin-bottom: 0;
-		}
-	}
+  .info-form {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    .el-form-item {
+      flex-basis: 48%;
+      margin-bottom: 0;
+    }
+  }
 }
 
 .action {
-	display: flex;
-	align-items: center;
-	justify-content: flex-end;
-	span {
-		color: #666;
-		font-size: 13px;
-		margin-right: 20px;
-	}
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  span {
+    color: #666;
+    font-size: 13px;
+    margin-right: 20px;
+  }
 }
 </style>
