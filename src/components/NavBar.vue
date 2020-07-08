70f1df5df1a276b6b6012fdb3b2ac27225a9e290
dengtao <dtqwe@qq.com>
2020-07-08 22:20:27 +0800

    增加logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="nav-bar">
-    <ul class="nav-menu">
-      <li
-        v-for="(item, index) of menuList"
-        :key="index"
-        :class="{ 'nav-menu-active': isMenuActive(item.type) }"
-        @click="$router.push(`/${item.url}`)"
-      >{{ item.name }}</li>
-    </ul>
+    <div class="nav-left">
+      <img src="../assets/images/logo.png" class="nav-logo" />
+      <ul class="nav-menu">
+        <li
+          v-for="(item, index) of menuList"
+          :key="index"
+          :class="{ 'nav-menu-active': isMenuActive(item.type) }"
+          @click="$router.push(`/${item.url}`)"
+        >{{ item.name }}</li>
+      </ul>
+    </div>
     <div class="nav-login">
       <div v-if="!this.isLogined" class="login-btn">
         <span @click="handleLogin">登录</span>
@@ -15,7 +18,7 @@
       </div>
       <el-dropdown v-else trigger="click">
         <span class="user-profile">
-          <img src="../assets/images/logined-logo.svg" class="user-avatar" />
+          <img src="../assets/images/user-logo.svg" class="user-avatar" />
           {{ this.username }}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
@@ -53,7 +56,7 @@ export default {
       loginOrRegister: "",
       menuList: [
         { name: "开发", url: "", type: "dev" },
-        { name: "瞎写", url: "essay", type: "essay" },
+        { name: "瞎写", url: "essay", type: "essay" }
         // { name: "瞎拍", url: "photo", type: "photo" }
       ]
     };
@@ -106,37 +109,47 @@ export default {
   background-color: #eee;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   &:hover {
     box-shadow: 0 2px 12px hsla(0, 0%, 69.4%, 0.4);
   }
-  .nav-menu {
-    height: 100%;
-    flex-basis: 30%;
-    margin: 0;
-    padding: 0;
-    list-style: none;
+  .nav-left {
     display: flex;
+    height: 100%;
     align-items: center;
-    padding: 0 30px;
-    li {
-      flex-basis: 100px;
+    padding-left: 30px;
+    .nav-logo {
+      width: 100px;
+      height: 40px;
+    }
+    .nav-menu {
       height: 100%;
+      margin: 0;
+      list-style: none;
+      padding: 0 30px;
       display: flex;
-      justify-content: center;
-      align-items: center;
-      color: #999;
-      cursor: pointer;
-      &:hover:not(.nav-menu-active) {
-        color: #666;
+      li {
+        width: 100px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        align-items: center;
+        color: #999;
+        cursor: pointer;
+        &:hover:not(.nav-menu-active) {
+          color: #666;
+        }
+      }
+      .nav-menu-active {
+        transition: all 0.25s ease-in-out;
+        background-color: #51defe;
+        background-image: linear-gradient(126deg, #51defe 0%, #1170f5 79%);
+        color: #ffffff;
       }
     }
-    .nav-menu-active {
-      transition: all 0.25s ease-in-out;
-      background-color: #51defe;
-      background-image: linear-gradient(126deg, #51defe 0%, #1170f5 79%);
-      color: #ffffff;
-    }
   }
+
   .nav-login {
     display: flex;
     justify-content: center;
@@ -158,7 +171,7 @@ export default {
       align-items: center;
       cursor: pointer;
       .user-avatar {
-        width: 25px;
+        width: 40px;
         margin-right: 10px;
       }
       i {
